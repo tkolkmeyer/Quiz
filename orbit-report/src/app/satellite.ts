7d7fdfe981dcfba74bdafa6a62cb24d1f89c0848
Taylor Kolkmeyer <taylorkolkmeyer@Taylors-Air.attlocal.net>
2020-11-08 17:33:46 -0600

    started on part 6

--- orbit-report/src/app/satellite.ts
+++ orbit-report/src/app/satellite.ts
@@ -11,5 +11,13 @@ export class Satellite {
         this.launchDate = launchDate;
         this.orbitType = orbitType;
         this.operational = operational;
-    };
-};
+    }
+
+    shouldShowWarning(): boolean {
+        if (this.type.toLowerCase() === 'space debris') {
+            return true
+        } else {
+            return false
+        }
+    }
+}
