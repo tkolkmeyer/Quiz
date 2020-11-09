8755304db75d7b1642b5da0e4ff9e62193c804cd
Taylor Kolkmeyer <taylorkolkmeyer@Taylors-Air.attlocal.net>
2020-11-09 14:48:07 -0600

    added orbit-counts component w/ css

--- orbit-report/src/app/app.component.ts
+++ orbit-report/src/app/app.component.ts
@@ -10,23 +10,42 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'orbit-report';
   sourceList: Satellite[];
+  displayList: Satellite[];
 
     constructor() {
       this.sourceList = [];
       let satellitesUrl = 'https://handlers.education.launchcode.org/static/satellites.json';
-  
+      this.displayList = [];
+      
       window.fetch(satellitesUrl).then(function(response){
         response.json().then(function(data){
-  
+
           let fetchedSatellites = data.satellites;
 
           for(let i = 0; i < fetchedSatellites.length; i++) {
             let satellite = new Satellite(fetchedSatellites[i].name, fetchedSatellites[i].type, fetchedSatellites[i].launchDate, fetchedSatellites[i].orbitType, fetchedSatellites[i].operational);
             this.sourceList.push(satellite);
           }
+
+          this.displayList = this.sourceList.slice(0);
+
         }.bind(this));
       }.bind(this));
     };
+
+    search(searchTerm: string): void {
+      let matchingSatellites: Satellite[] = [];
+      searchTerm = searchTerm.toLowerCase();
+      for(let i=0; i < this.sourceList.length; i++) {
+         let name = this.sourceList[i].name.toLowerCase();
+         if (name.indexOf(searchTerm) >= 0) {
+            matchingSatellites.push(this.sourceList[i]);
+         }
+      }
+      // assign this.displayList to be the array of matching satellites
+      // this will cause Angular to re-make the table, but now only containing matches
+      this.displayList = matchingSatellites;
+   }
  };
 
   
